3a01428c21ebb474581b04c8714df45b62a60ca6
Manuel Bogino <manuelbogino@gmail.com>
2024-07-22 10:58:01 -0300

    Add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const config = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Pokemon cards')
+    .setDescription('The pokemon cards API description')
+    .setVersion('1.0')
+    .addTag('cards')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(config.get('port') || 3000);
 }
 bootstrap();
